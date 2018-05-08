422b39c9cbcb405309113f8dbd064c1e6bf7c2eb
Muhammad Alam <alam@mmtutor.com>
2018-05-08 13:23:26 -0400

    continue auth-flow

--- src/components/NewUserForm/NewImageModal.js
+++ src/components/NewUserForm/NewImageModal.js
@@ -1,10 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Modal from "../Modal";
-import ImgEditor from "./ImgEditor";
-import {storage, auth, database} from "../../fire";
-import axios from 'axios'
-import Slider from "react-rangeslider";
-const _URL = window.URL || window.webkitURL;
+import { storage, auth } from "../../fire";
+import axios from "axios";
+import { CircularLoading } from "respinner";
 class NewImageModal extends Component {
   state = {
     picScale: 1,
@@ -12,71 +10,68 @@ class NewImageModal extends Component {
     zoomLevel: 0,
     selectedFile: null
   };
-  handleScroll = e => {
-    this.setState(oldState => {
-      let zoomLevel = Math.round(oldState.zoomLevel - e.deltaY);
-      // if (zoomLevel > 100 || zoomLevel < 0) return;
-      zoomLevel = zoomLevel > 100
-        ? 100
-        : zoomLevel < 0
-          ? 0
-          : zoomLevel;
-      const picScale = 1 + zoomLevel / 100 * 3;
-      return {zoomLevel, picScale};
-    });
-  };
-  handleSliderChange = e => {
-    this.setState({
-      zoomLevel: e,
-      picScale: 1 + e / 100 * 3
-    });
-  };
   fileSelectedHandler = e => {
+    this.setState({ selectedPic: "" });
     const file = e.target.files[0];
     console.log(file);
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = e => {
-      this.setState({
-        selectedFile: file,
-        selectedPic: e.target.result
-      }, () => console.log("done"));
+      console.log(e);
+      var i = new Image();
+
+      i.onload = () => {
+        const { width, height } = i;
+        if (height / width !== 1) {
+          console.log(height, width);
+          alert("picture must be square.");
+        } else {
+          this.setState(
+            {
+              selectedFile: file,
+              selectedPic: e.target.result
+            },
+            this.uploadHandler
+          );
+        }
+      };
+
+      i.src = e.target.result;
     };
   };
-  uploadHandler = async() => {
+  uploadHandler = async () => {
+    // this.setState({selectedPic: ''});
     console.log(this.state.selectedFile);
     console.log(this.state.selectedPic);
     const snapshot = await storage
       .ref(`users/${auth.currentUser.uid}`)
       .child(auth.currentUser.uid)
-      .put(this.state.selectedFile)
+      .put(this.state.selectedFile);
     console.log(snapshot.downloadURL);
-
-    const _userRef = database.ref(`_users/${auth.currentUser.uid}`);
 
     const promises = [];
     const username = this.props.username;
-    const changePhotoURLendpoint = 'https://us-central1-deuces-bovinecorvus.cloudfunctions.net/api/change_photo_url';
-    const idToken = await auth
-      .currentUser
-      .getIdToken(true);
+    const changePhotoURLendpoint =
+      "https://us-central1-deuces-bovinecorvus.cloudfunctions.net/api/change_photo_url";
+    const idToken = await auth.currentUser.getIdToken(true);
     console.log(snapshot);
     const downloadURL = snapshot.downloadURL;
-    if (username) {
-      const p2 = axios.post(changePhotoURLendpoint, {username, idToken, downloadURL})
-      promises.push(p2);
-    } else {
-      const p2 = axios.post(changePhotoURLendpoint, {idToken, downloadURL})
-      promises.push(p2);
-    }
-    Promise
-      .all(promises)
-      .then((e) => {
-        console.log('dooone~!', e);
-        this
-          .props
-          .updatePhotoURL(e[0].data.downloadURL)
-      })
+    // if (username) {
+    const p2 = axios.post(changePhotoURLendpoint, {
+      username,
+      idToken,
+      downloadURL
+    });
+    promises.push(p2);
+    // } else {
+    //   const p2 = axios.post(changePhotoURLendpoint, { idToken, downloadURL });
+    //   promises.unshift(p2);
+    // }
+    Promise.all(promises).then(e => {
+      console.log("dooone~!", e);
+      this.setState({ selectedPic: e[0].data.downloadURL });
+      this.props.updatePhotoURL(e[0].data.downloadURL);
+    });
   };
   render() {
     return (
@@ -86,24 +81,16 @@ class NewImageModal extends Component {
           <div className="NewUserForm-close" onClick={this.props.closeModal}>
             ✖
           </div>
-          <img src={this.state.selectedPic} alt=""/>
-          <input type="file" onChange={this.fileSelectedHandler}/>
-          <button onClick={this.uploadHandler}>Upload!</button>
-          {/* <ImgEditor
-            scale={this.state.picScale}
-            image={this.state.selectedPic}
-            alt=""
-          />
-          <div className="slider">
-            <Slider
-              min={0}
-              max={100}
-              value={this.state.zoomLevel}
-              onChange={this.handleSliderChange}
-              orientation="horizontal"
-              tooltip={false}
+          {this.state.selectedPic.length ? (
+            <img
+              src={this.state.selectedPic}
+              className="NewUserForm-chosenImg"
+              alt=""
             />
-          </div> */}
+          ) : (
+            <CircularLoading size={450} duration={1} stroke="#4197ff" />
+          )}
+          <input type="file" onChange={this.fileSelectedHandler} />
         </div>
       </Modal>
     );
--- src/components/NewUserForm/index.js
+++ src/components/NewUserForm/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
-import {RotateLoading} from "respinner";
+import React, { Component } from "react";
+import { RotateLoading } from "respinner";
 
 import Pencil from "../AssetsSVG/Pencil";
-import {database} from "../../fire";
+import { database } from "../../fire";
 import NewImageModal from "./NewImageModal";
 
 import "./NewUserForm.css";
@@ -10,99 +10,104 @@ import "./NewUserForm.css";
 // is prompted to choose a username input field with username that validates
 // username syntax smart submit button that validates username uniqueness
 
-const isValidUsername = str => str
-  .replace(/\s/g, "")
-  .toLowerCase()
-  .length > 5;
+const isValidUsername = str => str.replace(/\s/g, "").toLowerCase().length > 3;
 
 class NewUserForm extends Component {
   constructor(props) {
     super(props);
-    const {displayName} = props.user;
-    const username = displayName
-      .replace(/\s/g, "")
-      .slice(0, 20);
+    const { displayName } = props.user;
+    const username = displayName.replace(/\s/g, "").slice(0, 20);
     const buttonDisabled = !isValidUsername(displayName);
     this.state = {
       username,
       buttonDisabled,
-      buttonText: !buttonDisabled
-        ? "submit"
-        : "6+ characters",
+      buttonText: !buttonDisabled ? "submit" : "4+ characters",
       showModal: false,
       inputDisabled: false
     };
   }
-  closeModal = e => this.setState({showModal: false});
-  showModal = e => this.setState({showModal: true});
+  closeModal = e => this.setState({ showModal: false });
+  showModal = e => this.setState({ showModal: true });
   handleInputChange = e => {
     const username = e.target.value;
-    if (username.match(/\s/g)) 
-      return;
+    if (username.match(/\s/g)) return;
     const usernameKey = username.toLowerCase();
-    if (!usernameKey.match(/^[a-z0-9]{0,20}$/)) 
-      return;
+    if (!usernameKey.match(/^[a-z0-9]{0,20}$/)) return;
     const len = usernameKey.length;
-    const buttonDisabled = len < 6;
-    const buttonText = len < 6
-      ? "6+ characters"
-      : "submit";
-    this.setState({username, buttonDisabled, buttonText});
+    const buttonDisabled = len < 4;
+    const buttonText = len < 4 ? "4+ characters" : "submit";
+    this.setState({ username, buttonDisabled, buttonText });
   };
   handleSubmit = async e => {
-    const {buttonDisabled, username} = this.state;
-    if (buttonDisabled) 
-      return;
-    this.setState({buttonText: (<RotateLoading
-      duration={1}
-      stroke="#708090"
-      opacity={0.4}
-      size={30}
-      className="NewUserForm-spinner"/>), buttonDisabled: true, inputDisabled: true});
+    const { buttonDisabled, username } = this.state;
+    if (buttonDisabled) return;
+    this.setState({
+      buttonText: (
+        <RotateLoading
+          duration={1}
+          stroke="#708090"
+          opacity={0.4}
+          size={30}
+          className="NewUserForm-spinner"
+        />
+      ),
+      buttonDisabled: true,
+      inputDisabled: true
+    });
     const usernameKey = username.toLowerCase();
     const snapshot = await database
       .ref(`/users/${usernameKey}/public`)
       .once("value");
     if (snapshot.val()) {
-      this.setState({
-        buttonText: "name taken :(",
-        buttonDisabled: true,
-        inputDisabled: false
-      }, () => this.inputField.focus());
+      this.setState(
+        {
+          buttonText: "name taken :(",
+          buttonDisabled: true,
+          inputDisabled: false
+        },
+        () => this.inputField.focus()
+      );
     } else {
-      this
-        .props
-        .submitNewUserForm({username});
+      this.props.submitNewUserForm({ username });
     }
   };
   handleKeyPress = e => {
-    if (e.key === "Enter") 
-      this.handleSubmit();
-    };
+    if (e.key === "Enter") this.handleSubmit();
+  };
   render() {
-    const {signOut, user} = this.props;
-    const {photoURL, displayName} = user;
-    const {username, inputDisabled, buttonDisabled, buttonText, showModal} = this.state;
+    const { signOut, user, updatePhotoURL } = this.props;
+    const { photoURL, displayName } = user;
+    const {
+      username,
+      inputDisabled,
+      buttonDisabled,
+      buttonText,
+      showModal
+    } = this.state;
     return (
       <div className="NewUserForm">
-        {showModal && (<NewImageModal
-          selectedPic={photoURL}
-          username={username}
-          closeModal={this.closeModal}
-          updatePhotoURL={this.props.updatePhotoURL}/>)}
+        {showModal && (
+          <NewImageModal
+            selectedPic={photoURL}
+            username={username}
+            closeModal={this.closeModal}
+            updatePhotoURL={updatePhotoURL}
+          />
+        )}
         <img
           className="NewUserForm-profilePic"
           src={photoURL}
           alt={displayName}
-          onClick={this.showModal}/>
+          onClick={this.showModal}
+        />
         <div className="NewUserForm-changePicButton" onClick={this.showModal}>
-          <Pencil color={"#708090"}/>
+          <Pencil color={"#708090"} />
         </div>
         <div className="NewUserForm-label">Choose a username:</div>
         <input
-          className={`NewUserForm-textInput ${inputDisabled
-          ? "NewUserForm-textInputDisabled"
-          : ""}`}
+          className={`NewUserForm-textInput ${
+            inputDisabled ? "NewUserForm-textInputDisabled" : ""
+          }`}
           type="text"
           autoFocus
           spellCheck="false"
@@ -111,13 +116,15 @@ class NewUserForm extends Component {
           disabled={inputDisabled}
           onKeyPress={this.handleKeyPress}
           ref={input => {
-          this.inputField = input;
-        }}/>
+            this.inputField = input;
+          }}
+        />
         <div
-          className={`NewUserForm-button ${buttonDisabled
-          ? "NewUserForm-buttonDisabled"
-          : ""}`}
-          onClick={this.handleSubmit}>
+          className={`NewUserForm-button ${
+            buttonDisabled ? "NewUserForm-buttonDisabled" : ""
+          }`}
+          onClick={this.handleSubmit}
+        >
           {buttonText}
         </div>
         <div className="NewUserForm-signoutText">
