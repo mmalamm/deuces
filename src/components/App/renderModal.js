638750babac9118f8ff2d7b6c8321f5f7405e922
Muhammad Alam <alam@mmtutor.com>
2018-07-09 09:40:51 -0400

    refactor rendermodal

--- src/components/App/renderModal.js
+++ src/components/App/renderModal.js
@@ -4,8 +4,8 @@ import ChangePicForm from "../../containers/ChangePicForm";
 import { modalize } from "../Modal/Modal";
 
 const modals = {
-  NEW_GAME_FORM: modalize(<NewGameForm />),
-  CHANGE_PIC_FORM: modalize(<ChangePicForm />)
+  NEW_GAME_FORM: <NewGameForm />,
+  CHANGE_PIC_FORM: <ChangePicForm />
 };
 
-export default modalName => modals[modalName];
+export default modalName => modalize(modals[modalName]);
